651319d8c4c10c20cf4318cbb5b687c994212a8b
Tyler Croley <tjcroley@outlook.com>
2020-03-06 18:44:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
     result
   end
   def self.find_or_create_by_name(title)
-    result = self.find_or_create_by_name(title)
+    result = self.find_by_name(title)
     if result 
       result
     else
